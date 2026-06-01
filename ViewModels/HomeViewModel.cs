6dadb178e934a2c611484e202951eec5100e4ee6
Abrar Hussein <abrarhussein3434@gmail.com>
2026-06-01 19:07:53 +0300

    Update layout and styling

---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CourseManagementSystem.Models;
+
+namespace CourseManagementSystem.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Course> FeaturedCourses { get; set; } = new();
+        public List<Course> TrendingCourses { get; set; } = new();
+        public List<Course> NewCourses { get; set; } = new();
+        public List<Category> Categories { get; set; } = new();
+        public List<Review> TopReviews { get; set; } = new();
+
+        public int TotalCourses { get; set; }
+        public int TotalStudents { get; set; }
+        public int TotalInstructors { get; set; }
+        public int TotalCategories { get; set; }
+    }
+}
