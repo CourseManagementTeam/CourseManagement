2c7006b8c111e559ad5afa9cf737883da8344285
Basher <bashiralansary306@gmail.com>
2026-06-02 23:23:30 +0300

    Implement Sections Index action

--- Controllers/SectionController.cs
+++ Controllers/SectionController.cs
@@ -11,6 +11,15 @@ namespace CourseManagementSystem.Controllers
         {
             _sectionRepository = sectionRepository;
         }
-        
+
+        // GET: Section/Index?courseId=1
+        public IActionResult Index(int courseId)
+        {
+            var sections = _sectionRepository.GetSectionsByCourseId(courseId);
+            return View(sections);
+        }
+
+       
+
     }
 }
