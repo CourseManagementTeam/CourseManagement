07f03f7eb7347e61c82cb81ad2f46f7094becb45
Basher <bashiralansary306@gmail.com>
2026-06-02 22:43:50 +0300

    Add Section and Lesson controllers with dependency injection

---
+++ Controllers/SectionController.cs
@@ -0,0 +1,16 @@
+﻿using CourseManagementSystem.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseManagementSystem.Controllers
+{
+    public class SectionController : Controller
+    {
+        private readonly ISectionRepository _sectionRepository;
+
+        public SectionController(ISectionRepository sectionRepository)
+        {
+            _sectionRepository = sectionRepository;
+        }
+        
+    }
+}
