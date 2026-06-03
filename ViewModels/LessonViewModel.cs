96a7f198654035e52b6f765dd979364d11ef5426
Basher <bashiralansary306@gmail.com>
2026-06-04 01:06:33 +0300

    add LessonViewModel for validation

---
+++ ViewModels/LessonViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseManagementSystem.ViewModels
+{
+    public class LessonViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Lesson title is required")]
+        [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
+        public string Title { get; set; }
+
+        [Required]
+        public int SectionId { get; set; }
+    }
+}
