f7862e6fad2ecf88585955b4a7e933057a42c3b4
Basher <bashiralansary306@gmail.com>
2026-06-04 01:22:04 +0300

    add lesson create action and its view

--- Controllers/LessonController.cs
+++ Controllers/LessonController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CourseManagementSystem.Models;
+using CourseManagementSystem.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CourseManagementSystem.Controllers
 {
@@ -21,5 +24,43 @@ namespace CourseManagementSystem.Controllers
             return View(lessons);
         }
 
+        // GET: Lesson/Create?sectionId=1
+        [HttpGet]
+        public IActionResult Create(int sectionId)
+        {
+            var vm = new LessonViewModel
+            {
+                SectionId = sectionId
+            };
+            return View(vm);
+        }
+
+        // POST: Lesson/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(LessonViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            try
+            {
+                var newLesson = new Lesson
+                {
+                    Title = vm.Title,
+                    SectionId = vm.SectionId
+                };
+
+                _lessonRepository.Add(newLesson);
+                _lessonRepository.Save();
+
+                return RedirectToAction(nameof(Index), new { sectionId = vm.SectionId });
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError(string.Empty, "Unable to save. The parent section could not be found.");
+                return View(vm);
+            }
+        }
     }
 }
